2a25517c2342a7481f32033bd17bb6df8aaf998c
wukong-1213 <wukong.monkeyking.1213@gmail.com>
2024-11-23 07:50:50 +0000

    feat: script 수정

--- start_monitoring.sh
+++ start_monitoring.sh
@@ -1,4 +1,8 @@
-# bin/bash
+#!/bin/bash
+
+# JAVA_HOME 설정
+export JAVA_HOME="/home/wukong/.jdks/temurin-17.0.13"
+
 # 이전 실행 중인 프로세스 종료
 ps -ax | grep 'monitoring.*jar' | awk '{print $1}' | xargs kill 2>/dev/null || true
 
@@ -7,10 +11,10 @@ ps -ax | grep 'monitoring.*jar' | awk '{print $1}' | xargs kill 2>/dev/null || t
 
 # 빌드 성공 확인
 if [ $? -eq 0 ]; then
-   # nohup으로 실행
-   nohup ~/.jdks/temurin-17.0.13/bin/java -jar build/libs/monitoring-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
-   echo "Application started with PID $!"
+    # nohup으로 실행
+    nohup $JAVA_HOME/bin/java -jar build/libs/monitoring-0.0.1-SNAPSHOT.jar > app.log 2>&1 &
+    echo "Application started with PID $!"
 else
-   echo "Build failed"
-   exit 1
+    echo "Build failed"
+    exit 1
 fi
